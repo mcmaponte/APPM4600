b38dc18828f3407024d63808481e4929f7854ea3
mcmaponte <Matthew.Menendez-Aponte@colorado.edu>
2024-04-02 11:13:50 -0600

    lab10 Lab

--- Labs/Lab10/Lab10.py
+++ Labs/Lab10/Lab10.py
@@ -1,6 +1,9 @@
 import numpy as np
 import scipy as sc
 import matplotlib.pyplot as plt
+import math
+from scipy.integrate import quad
+
 
 def eval_legendre(n, x):
     # nth order
@@ -36,6 +39,91 @@ def prelabdriver():
     print('Mine: ', p_mine)
 
 
+def eval_coeff(n, num, den, range):
+    return quad(num, range[0], range[1], args=(n,)) / quad(den, range[0], range[1], args=(n,))
+
+def Problem1Driver():
+    def f(x):
+        return x
+    def w(x):
+        return 1
+
+
+
+
+
+def eval_legendre_expansion(f, a, b, w, n, x):
+    # This subroutine evaluates the Legendre expansion
+    # Evaluate all the Legendre polynomials at x that are needed
+    # by calling your code from prelab
+    # initialize the sum to 0
+    pval = 0.0
+    for j in range(0, n+1):
+        def phi_j(x):
+            p = eval_legendre(j, x)
+            return p[-1]
+        # define numerator
+        def num_integrand(x):
+            return f(x) * phi_j(x) * w(x)
+        # define denominator
+        def den_integrand(x):
+            return phi_j(x) ** 2 * w(x)
+
+
+        # use the quad function from scipy to evaluate normalizations
+        denominator, err = quad(den_integrand, a, b)
+        # make a function handle for phi_j(x)*f(x)*w(x)/norm_fac
+        # use the quad function from scipy to evaluate coeffs
+        numerator, err = quad(num_integrand, a, b)
+
+        aj = numerator / denominator
+        # accumulate into pval
+        pval = pval + aj*phi_j(x)
+    return pval
+
+def driver():
+#function you want to approximate
+    # Problem 2
+    # f = lambda x: math.exp(x)
+    # Problem 3
+    def f(x): return 1 / (1 + x**2)
+    # Interval of interest
+    a = -1
+    b = 1
+    # weight function
+    w = lambda x: 1.
+    # order of approximation
+    n = 2
+    # Number of points you want to sample in [a,b]
+    N = 1000
+    xeval = np.linspace(a, b, N+1)
+    pval = np.zeros(N+1)
+
+    for kk in range(N+1):
+        pval[kk] = eval_legendre_expansion(f, a, b, w, n, xeval[kk])
+    ''' create vector with exact values'''
+    fex = np.zeros(N+1)
+    for kk in range(N+1):
+        fex[kk] = f(xeval[kk])
+    plt.figure(1)
+    plt.plot(xeval, fex,'ro-', label= 'f(x)')
+    plt.plot(xeval, pval,'bs--',label= 'Expansion')
+    plt.legend()
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('3) f(x) vs Polynomial Approximation')
+    plt.grid(True)
+    plt.savefig('3Curves.png')
+    plt.figure(2)
+    err_l = abs(pval-fex)
+    plt.semilogy(xeval,err_l,'ro--',label='error')
+    plt.xlabel('x')
+    plt.ylabel('Log Error')
+    plt.title('3) Error between Function and Approximation')
+    plt.grid(True)
+    plt.legend()
+    plt.savefig('3Error.png')
+
 if __name__ == '__main__':
-    prelabdriver()
+    driver()
 
